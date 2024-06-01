aaa486bdeebf6ea30aa73d3575271e1a3ded19df
eduardohen1 <eduardohmferreira@gmail.com>
2024-06-01 11:42:13 -0300

    Aula 01_06_24 Iniciando java - Operadores de looping

--- HelloJP/src/br/com/ehmf/Principal.java
+++ HelloJP/src/br/com/ehmf/Principal.java
@@ -26,13 +26,14 @@ public class Principal {
 			System.out.println("* Filme Adulto (" + idade + ")");
 		}
 		
-		idade = 4;
+		idade = 3;
 		switch(idade) {
 			case 2:
 				System.out.println("- Idade igual a 2");
+				break;
 			case 3:
 			case 4:
-				System.out.println("- Idade igual a 4");
+				System.out.println("- Idade igual a 3 e 4");
 				break;
 			case 5:
 			case 10: 
@@ -47,6 +48,38 @@ public class Principal {
 			default:
 				System.out.println("- Outra idade");
 				break;
+		}
+		
+		// trabalhando com looppings:
+		// criar uma contagem de 0 até 20
+		int max = 21;
+		for(int numero = 0; numero < max; numero++) {
+			//System.out.print(numero);			
+			// se o número for par, exibe um * entre o número, caso não
+			//  exibe somente o número.
+			if(numero % 2 == 0) {
+				System.out.print("*" + numero + "*");
+			}else {
+				System.out.print(numero);
+			}			
+			if(numero < (max - 1)) {
+				System.out.print(", ");
+			}
+		}
+		
+		//while
+		int i = 0;
+		while(i <= 10) {
+			System.out.println("Escrevendo i: " + i);
+			
+			switch(i) {
+				case 1:
+					System.out.println(" Achei o 1!!!");
+					break;
+				default:
+					System.out.print("----");
+			}			
+			i++;
 		}
 		
 	}
