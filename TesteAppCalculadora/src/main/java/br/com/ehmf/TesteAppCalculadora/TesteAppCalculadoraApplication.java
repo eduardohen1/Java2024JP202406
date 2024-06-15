05b2e806e9ef5588ceca06a5cabdcfc259d928f2
eduardohen1 <eduardohmferreira@gmail.com>
2024-06-15 09:09:50 -0300

    Aula 15_06_24 Iniciando projeto Spring Calculadora

---
+++ TesteAppCalculadora/src/main/java/br/com/ehmf/TesteAppCalculadora/TesteAppCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.ehmf.TesteAppCalculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAppCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteAppCalculadoraApplication.class, args);
+	}
+
+}
---
+++ TesteAppCalculadora/src/test/java/br/com/ehmf/TesteAppCalculadora/TesteAppCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ehmf.TesteAppCalculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAppCalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
