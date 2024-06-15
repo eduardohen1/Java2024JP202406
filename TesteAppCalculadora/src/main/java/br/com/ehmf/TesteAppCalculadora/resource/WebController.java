5ff2840972332733f4433e99f1d85a28aa699ce3
eduardohen1 <eduardohmferreira@gmail.com>
2024-06-15 09:44:00 -0300

    Aula 15_06_24 Iniciando projeto Spring Calculadora - 01

--- TesteAppCalculadora/src/main/java/br/com/ehmf/TesteAppCalculadora/resource/WebController.java
+++ TesteAppCalculadora/src/main/java/br/com/ehmf/TesteAppCalculadora/resource/WebController.java
@@ -3,12 +3,27 @@ package br.com.ehmf.TesteAppCalculadora.resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
 	
 	@GetMapping("/")
 	public String showForm(Model model) {
+		return "index";
+	}
+	
+	@PostMapping("/")
+	public String handleFormSubmission(
+			@RequestParam String modelOperacoes,
+			@RequestParam String valor01,
+			@RequestParam String valor02,
+			Model model) {
+		
+		System.out.println("modelOperacoes: " + modelOperacoes + ", " +
+						   "valor01: " + valor01 + ", " +
+				           "valor02: " + valor02);
 		return "index";
 	}
 	
