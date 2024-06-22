d75292ca694912b33afeff7ac67fe13279846a40
eduardohen1 <eduardohmferreira@gmail.com>
2024-06-22 15:52:48 -0300

    Aula 22_06_24 JWT

---
+++ AppProdutos/src/main/java/br/com/ehmf/AppProdutos/configuration/SecurityConfig.java
@@ -0,0 +1,32 @@
+package br.com.ehmf.AppProdutos.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.authorizeHttpRequests(
+					(authz) -> authz.requestMatchers(
+							"/v3/api-docs/**",
+							"/swagger-ui/**",
+							"/swagger-ui.html",
+							"/swagger-resources/**",
+							"/webjars/**"
+							).permitAll().anyRequest().authenticated()
+					);
+		return http.build();
+	}
+	
+}
